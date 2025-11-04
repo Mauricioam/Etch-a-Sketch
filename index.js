9a4925f6d4c6a68c84250b8c2865debb6cdfd2a3
Mauricioam <mauricioam91@gmail.com>
2025-11-04 20:46:25 -0300

    fix div issue added rmv function that remove all the divs from container user can change resolution

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const container = document.querySelector("#container");
 const resolutionBtn = document.querySelector("#change-resolution");
 
+const rmvContainer = (elem) => {
+  while (elem.firstChild) {
+    elem.removeChild(elem.firstChild);
+  }
+};
+
 const requestPrompt = () => {
+  rmvContainer(container);
   let userInput = prompt("Enter a value", 16);
   if (userInput >= 100 && userInput == 0) {
     alert("Have to be below 100 and can't be zero");
     requestPrompt();
   }
+
   createDivs(userInput);
 };
 
